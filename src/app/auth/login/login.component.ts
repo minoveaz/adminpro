19013b8ae52ce7baeca9b03cc3aee21544edaabb
Miller Camilo Vega Diaz <ing.miller.vega@gmail.com>
2021-11-06 22:40:42 +0100

    login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -16,17 +18,25 @@ export class LoginComponent  {
 
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]],
+    remember: [false]
   });
+  
 
   constructor( private router: Router,
-               private fb: FormBuilder) { }
+               private fb: FormBuilder,
+               private userService: UserService) { }
                
-
-  
   login(){
 
-    console.log(this.loginForm.value)
-
+    this.userService.login(this.loginForm.value)
+        .subscribe( resp => {
+          if(this.loginForm.get('remember')?.value){
+            localStorage.setItem('email', this.loginForm.get('email')?.value)
+          }
+        }, (err) =>{
+          // if an error
+          Swal.fire('Error', err.error.msg,'error');
+        })
     //this.router.navigateByUrl('/')
   }
 }
